c146c00b35319b2e210f8dfe0d69c27330a0e2bf
aquaktus <aquaktus@yahoo.co.uk>
2019-10-15 12:54:24 +0100

    Adding docker env scripts

---
+++ docker/start_from_dockerfile.sh
@@ -0,0 +1,7 @@
+if [ -z "$1" ]; then echo "No args, please specify path to Dockerfile"; exit 0; fi
+echo "taking Dockerfile from:" $1
+docker build -t deep_phd_env_img -f $1 .
+docker run --name deep_phd_env -it -p 8888:8888 -v $PWD/../:/my_shared/ -d deep_phd_env_img 
+docker exec -it deep_phd_env bash
+docker stop deep_phd_env
+docker rm deep_phd_env
